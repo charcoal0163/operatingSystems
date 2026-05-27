18cce5da2c259bb7db9da645a6fd24740b4162d1
charcoal <202420163@students.asu.edu.jo>
2026-05-27 20:06:38 +0300

    fixing resourceRelease

--- algorithm.java
+++ algorithm.java
@@ -103,89 +103,29 @@ class algorithm{
         }
         sequence = tempSeq; 
         return true;
-    }
-    public void resourceRelease(int processID, int[] release){
-        if(processID < 0 || processID >= processes){
-            System.out.println("Invalid Process ID.");
+    }    
+    public void resourceRelease(){
+        if(sequence == null || sequence.length == 0){
+            System.out.println("Execution halted. Safe sequence unverified.");
             return;
-        }
-        if(release.length != resources){
-            System.out.println("Invalid release array length.");
-            return;
-        }
-        System.out.println("Releasing resources of process P" + processID);
-        for(int i = 0; i < resources; i++){
-            if(release[i] < 0){
-                System.out.println("Invalid. Cannot release negative resource amounts.");
-                return;
-            }
-            if(release[i] > alloc[processID][i]){
-                System.out.println("Invalid. Process is attempting to release more resources than allocated.");
-                return;
-            }
-        }
-        for (int i = 0; i < resources; i++) {
-            alloc[processID][i] -= release[i];
-            available[i] += release[i];
-            need[processID][i] += release[i];
         }
 
-        System.out.println("Updated Allocation Matrix:");
-        for (int i = 0; i < processes; i++) {
-            for (int j = 0; j < resources; j++) {
-                System.out.print(alloc[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println("Updated available resources:");
-        for(int i = 0; i < resources; i++){
-            System.out.print(available[i] + " ");
-        }
-        System.out.println();
-        System.out.println("Updated Need Matrix:");
         for(int i = 0; i < processes; i++){
+            int processID = sequence[i];
+            System.out.println("Releasing allocated resources for P" + processID);
+
             for(int j = 0; j < resources; j++){
-                System.out.print(need[i][j] + " ");
+                available[j] += alloc[processID][j];
+                need[processID][j] += alloc[processID][j];
+                alloc[processID][j] = 0;
+            }
+
+            System.out.print("Available Vector State: ");
+            for(int j = 0; j < resources; j++){
+                System.out.print(available[j] + " ");
             }
             System.out.println();
         }
-        System.out.println("Resources released successfully.");        
+        System.out.println("System resource reclamation complete.");
     }
-    /*public void resourceRelease(int processID, int[] release){
-        System.out.println("Releasing resources of process P" + processID);
-        for(int i = 0; i < resources; i++){
-            if(release[i] > alloc[processID][i]){
-                System.out.println("Invalid. Process is attempting to release more resources than allocated.");
-                return;
-            }
-        }
-        // updating the matrices
-        for (int i = 0; i < resources; i++) {
-            alloc[processID][i] -= release[i];
-            available[i] += release[i];
-            need[processID][i] += release[i];
-        }
-        // print allocation matrix after release (decreased)
-        System.out.println("Updated Allocation Matrix:");
-        for (int i = 0; i < processes; i++) {
-            for (int j = 0; j < resources; j++) {
-                System.out.print(alloc[i][j] + " ");
-            }
-            System.out.println();
-        }
-        // print available vector after release (increased)
-        System.out.println("Updated available resources:");
-        for(int i = 0; i < resources; i++){
-            System.out.print(available[i] + " ");
-        }
-        // print need matrix after release (increased)
-        System.out.println("Updated Need Matrix:");
-        for(int i = 0; i < processes; i++){
-            for(int j = 0; j < resources; j++){
-                System.out.print(need[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println("Resources released successfully.");        
-    }*/
 }
